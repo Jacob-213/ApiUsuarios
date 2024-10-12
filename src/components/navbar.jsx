c7de23fe9394dff95e43d8dedb2cc8e1c4a7379f
Jacob-213 <jacobms.ti23@utsjr.edu.mx>
2024-10-11 21:53:20 -0600

    first commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar(){
     return(
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div classNameName="container-fluid">
+            <div className="container-fluid">
             <Link className="navbar-brand" href="#">Navbar</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -15,7 +15,10 @@ export default function Navbar(){
             <Link className="nav-link active" aria-current="page" href="#">Home</Link>
             </li>
             <li className="nav-item">
-            <Link className="nav-link" href="#">Link</Link>
+            <Link className="nav-link" href="/chat">Chat</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" href="/universidades">Universidades</Link>
             </li>
             <li className="nav-item dropdown">
             <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
